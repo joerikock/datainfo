d3e15e61c9deaf0a71b4fa59c2c6fac6e6779834
joerikock <joerikock@live.nl>
2014-05-06 16:32:25 +0200

    Result 3b
    Stap 1: Shunting Rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,7 @@
 SELECT p.pid FROM Person p
 WHERE EXISTS (
-	SELECT * FROM Acts a
+	SELECT * FROM Acts a, Movie m
 	WHERE a.pid = p.pid
-	AND EXISTS (
-		SELECT * FROM Movie m
-		WHERE m.mid = a.mid
-		AND m.name = 'Back to the Future'
-	)
+	AND m.mid = a.mid
+	AND m.name = 'Back to the Future'
 );
