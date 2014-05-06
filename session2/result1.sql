65c4bc3e31b01797a9e2f8b52d89fbdd74ad1c6c
joerikock <joerikock@live.nl>
2014-05-06 15:24:24 +0200

    d

--- session2/result1.sql
+++ session2/result1.sql
@@ -1,12 +1,12 @@
-INSERT INTO Problem1 
-VALUES 	('a', false, "Er kunnen meerdere voertuigen zijn met dezelfde capaciteit (punt 3)."),
-		('b', true, "Van elk voertuig is de capaciteit bekend (punt 3)."),
-		('c', false, "Een factuur, horende bij een gegeven afleveradres, kan meerdere pakjes bevatten (punt 7, 8)."),
-		('d', true, "Als je het pakje weet, weet de de factuur. Als je de factuur weet, weet je ook het afleveradres (punt c, 8)."),
-		('e', true, "Als je het pakje weet, weet je ook wie de ontvanger hiervan is. Tevens weet je dan ook hoe duur dit pakje is (punt 4, 5)."),
-		('f', false, "Een bestuurder kan gerechtigd zijn om meerdere voertuigen te besturen (punt 2)."),
-		('g', false, "Een bestuurder kan gerechtigd zijn om meerdere voertuigen te besturen (punt 2)."),
-		('h', false, "Voertuig is geen primary key waarmee de bestuurder verkregen kan worden. Er kunnen namelijk meerdere bestuurders in een voertuig gereden hebben."),
-		('i', true, "Een voertuig heeft altijd 1 bepaalde capaciteit, dus dit is geen probleem."),
-		('j', false, "Voertuig is geen primary key waarmee de bestuurder verkregen kan worden. Er kunnen namelijk meerdere bestuurders in een voertuig gereden hebben.")
+INSERT INTO Problem1 (task, result, motivation)
+VALUES 	('a', 0, "Er kunnen meerdere voertuigen zijn met dezelfde capaciteit (punt 3)."),
+		('b', 1, "Van elk voertuig is de capaciteit bekend (punt 3)."),
+		('c', 0, "Een factuur, horende bij een gegeven afleveradres, kan meerdere pakjes bevatten (punt 7, 8)."),
+		('d', 1, "Als je het pakje weet, weet de de factuur. Als je de factuur weet, weet je ook het afleveradres (punt c, 8)."),
+		('e', 1, "Als je het pakje weet, weet je ook wie de ontvanger hiervan is. Tevens weet je dan ook hoe duur dit pakje is (punt 4, 5)."),
+		('f', 0, "Een bestuurder kan gerechtigd zijn om meerdere voertuigen te besturen (punt 2)."),
+		('g', 0, "Een bestuurder kan gerechtigd zijn om meerdere voertuigen te besturen (punt 2)."),
+		('h', 0, "Voertuig is geen primary key waarmee de bestuurder verkregen kan worden. Er kunnen namelijk meerdere bestuurders in een voertuig gereden hebben."),
+		('i', 1, "Een voertuig heeft altijd 1 bepaalde capaciteit, dus dit is geen probleem."),
+		('j', 0, "Voertuig is geen primary key waarmee de bestuurder verkregen kan worden. Er kunnen namelijk meerdere bestuurders in een voertuig gereden hebben.")
 ;
---
+++ session2/result2.sql
@@ -0,0 +1,8 @@
+INSERT INTO R (A, B, C, D, E, F)
+VALUES 	(),
+;
+
+INSERT INTO F (x, y, violation, motivation)
+VALUES 	('E', 'D', 1, "Als je E weet, weet je D en A, maar niet B en C"),
+		('D', 'A', 1, "Als je D weet, weet je alleen A. B, C, E en F weet je allemaal niet."),
+;
