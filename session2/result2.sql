d050365acb48d65eff4de4752f0dac4eac260579
joerikock <joerikock@live.nl>
2014-05-06 15:38:21 +0200

    asdfghjkl

--- session2/result2.sql
+++ session2/result2.sql
@@ -2,3 +2,12 @@ INSERT INTO F (x, y, violation, motivation)
 VALUES 	('E', 'D', 1, "Als je E weet, weet je D en A, maar niet B en C"),
 		('D', 'A', 1, "Als je D weet, weet je alleen A. B, C, E en F weet je allemaal niet.")
 ;
+
+INSERT INTO F1 (x, y, violation, motivation)
+VALUES 	('E', 'D', 0, ""),
+		('D', 'A', 0, ""),	
+;
+
+INSERT INTO F1 (x, y, violation, motivation)
+VALUES 	('EBC', 'EF', 0, ""),
+;
