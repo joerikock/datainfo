eaa947ee99d90b4970f8277f0ea11664cb284edd
joerikock <joerikock@live.nl>
2014-05-06 15:53:06 +0200

    Exercise 2 taak c

--- session2/result2.sql
+++ session2/result2.sql
@@ -8,7 +8,15 @@ VALUES 	('E', 'D', 0, ""),
 		('D', 'A', 0, "")
 ;
 
-INSERT INTO F1 (x, y, violation, motivation)
+INSERT INTO F2 (x, y, violation, motivation)
 VALUES 	('ABC', 'EF', 0, ""),
 		('E', 'A', 1, "Als je E weet, kun je niet B en C weten.")
+;
+
+INSERT INTO F2a (x, y, violation, motivation)
+VALUES 	('E', 'A', 0, "")
+;
+
+INSERT INTO F2b (x, y, violation, motivation)
+VALUES 	('EBC', 'EF', 0, "")
 ;
